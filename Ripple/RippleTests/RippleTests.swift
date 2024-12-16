95514fd41b119d3b19e8a2fe90b889333f542660
ashzhang1 <ashzhang1@gmail.com>
2024-12-16 22:27:48 +1100

    intial commit with basic project setup

---
+++ Ripple/RippleTests/RippleTests.swift
@@ -0,0 +1,17 @@
+//
+//  RippleTests.swift
+//  RippleTests
+//
+//  Created by Ashley Zhang on 16/12/2024.
+//
+
+import Testing
+@testable import Ripple
+
+struct RippleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
