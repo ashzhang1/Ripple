1cad69ff615ca0d225419cea2101280edf03c03f
ashzhang1 <ashzhang1@gmail.com>
2024-12-17 22:04:36 +1100

    made data model entities

---
+++ Ripple/Ripple/Models/ReflectionData.swift
@@ -0,0 +1,8 @@
+//
+//  ReflectionData.swift
+//  Ripple
+//
+//  Created by Ashley Zhang on 17/12/2024.
+//
+
+import Foundation
---
+++ Ripple/Ripple/Models/StepData.swift
@@ -0,0 +1,8 @@
+//
+//  StepData.swift
+//  Ripple
+//
+//  Created by Ashley Zhang on 17/12/2024.
+//
+
+import Foundation
